a9b3d0d89d909869bdb88cb5b59a2995af5d3999
David Busse <dpb@essub.com>
2012-02-25 11:08:08 -0600

    use configuration_menu_item when adding to the admin configuration menu

--- app/overrides/admin_configurations_menu.rb
+++ app/overrides/admin_configurations_menu.rb
@@ -1,11 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                      :name => "admin_configurations_menu",
                      :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text =>
-                     %[
-                       <tr>
-                         <td><%= link_to t("mailchimp_settings"), admin_mail_chimp_settings_path %></td>
-                         <td><%= t("mailchimp_settings_description") %></td>
-                       </tr>
-                     ],
+                     :text => "<%= configurations_menu_item(t('mailchimp_settings'), admin_mail_chimp_settings_path, t('mailchimp_settings_description')) %>",
                      :disabled => false)
