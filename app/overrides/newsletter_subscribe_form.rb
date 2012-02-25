af89040d7f5f6d7ee09d6cf8f88de51d701e97fe
David Busse <dpb@essub.com>
2012-02-25 11:41:47 -0600

    change underbars to dashes for ids in override matchers

--- app/overrides/newsletter_subscribe_form.rb
+++ app/overrides/newsletter_subscribe_form.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "newsletter_subscribe_form",
-                     :insert_after => "[data-hook='footer_right'], #footer_right[data-hook]",
+                     :insert_after => "[data-hook='footer-right'], #footer-right[data-hook]",
                      :partial => "shared/newsletter_subscribe_form",
                      :disabled => false)
