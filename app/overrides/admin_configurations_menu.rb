19e95d62ac671aefa0043b9907bf857f8870949c
David Busse <dpb@essub.com>
2012-02-25 11:16:36 -0600

    append to spree routes

--- app/overrides/admin_configurations_menu.rb
+++ app/overrides/admin_configurations_menu.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
                      :name => "admin_configurations_menu",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
                      :text => "<%= configurations_menu_item(t('mailchimp_settings'), admin_mail_chimp_settings_path, t('mailchimp_settings_description')) %>",
                      :disabled => false)
