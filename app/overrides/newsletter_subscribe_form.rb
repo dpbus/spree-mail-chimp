2ed22699e707b5cb32bd38819ee80e953568595d
David Busse <dpb@essub.com>
2012-02-25 11:34:24 -0600

    override application layout intead of footer partial, move subscribe form to the right footer

--- app/overrides/newsletter_subscribe_form.rb
+++ app/overrides/newsletter_subscribe_form.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/shared/_footer",
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "newsletter_subscribe_form",
-                     :insert_after => "[data-hook='footer_left'], #footer_left[data-hook]",
+                     :insert_after => "[data-hook='footer_right'], #footer_right[data-hook]",
                      :partial => "shared/newsletter_subscribe_form",
                      :disabled => false)
