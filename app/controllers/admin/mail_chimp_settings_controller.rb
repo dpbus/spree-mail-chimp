22d3b48f2f04748bc8851b7a5e8589637264b596
David Busse <dpb@essub.com>
2012-02-25 20:52:38 -0600

    propery namespace inherited controller name

--- app/controllers/admin/mail_chimp_settings_controller.rb
+++ app/controllers/admin/mail_chimp_settings_controller.rb
@@ -1,4 +1,4 @@
-class Admin::MailChimpSettingsController < Admin::BaseController
+class Admin::MailChimpSettingsController < Spree::Admin::BaseController
 
   def update
     Spree::Config.set(params[:preferences])
